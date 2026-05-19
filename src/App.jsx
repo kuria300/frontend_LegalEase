983074bd0353e546cff473e6090ed0867f6e4d5a
Kuria300 <eugenekuria66@gmail.com>
2026-05-19 14:37:30 +0300

    LAP-52 Build OTP Verification Modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/home/HomePage"
 import RegisterPage from "./pages/register/RegisterPage"
 import LoginPage from "./pages/login/LoginPage"
 import { Icon } from "lucide-react"
+import Verifyotp from "./pages/verifyotp/Verifyotp"
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
        <Route path="/" element={<HomePage />}/>
        <Route path='/signup' element={<RegisterPage />}/>
        <Route path='/login' element={<LoginPage />}/>
+       <Route path='/verifyOtp' element={<Verifyotp />}/>
      </Routes>
     </>
   )
