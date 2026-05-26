d90720d56347c85b345ac8b19de32572ff84fa72
Kuria300 <eugenekuria66@gmail.com>
2026-05-26 14:59:58 +0300

    made changes displaytime

---
+++ src/utils/displayTime.js
@@ -0,0 +1,7 @@
+export const formatTime = (timeValue) => {
+  return new Date(timeValue).toLocaleTimeString('en-KE', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Africa/Nairobi', 
+  });
+};
